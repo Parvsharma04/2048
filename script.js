5353fb95133a7eaf06d6b78a51ca88374840e6f1
Parv Sharma <sharmaparv.2004@gmail.com>
2024-02-21 13:15:32 +0530

    Board Rendered

--- script.js
+++ script.js
@@ -38,7 +38,47 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     function renderBoard(){
         for(let i = 0; i<size;  i++){
-            for(let j = 0; j<size; j++)
+            for(let j = 0; j<size; j++){
+                const cell = document.querySelector(`[data-row="${i}"data-col="${j}"]`)
+
+                const preValue = cell.dataset.value
+                const currentValue = board[i][j]
+
+                if(cell.dataset.value !== 0){
+                    cell.dataset.value = currentValue
+                    cell.currentValue = currentValue
+
+                    if(currentValue !== parseInt(preValue) && !cell.classList.contains('new-tile')){
+                        cell.classList.add('merged-tile')
+                    }
+                }
+                else{
+                    cell.textContent = ''
+                    delete cell.dataset.value
+                    cell.classList.remove('merged-tile', 'new-tile')
+                }
+            }
+        }
+
+        setTimeout(()=>{
+            const cells = document.querySelectorAll('grid-cell')
+            cells.forEach(cell=>{
+                cell.classList.remove('merged-tile', 'new-tile')
+            })
+        }, 300)
+    }
+
+    function placeRandom(){
+        const available = []
+        for (let i = 0; i < size; i++) {
+            for(let j = 0; j<size; j++){
+                if(board[i][j] === 0){
+                    available.push({x: i, y: j})
+                }
+            }
+        }
+        if(available.length > 0 ){
+            
         }
     }
 })
