b78edfb610bf721301557529a22e557072cc5159
Parv Sharma <sharmaparv.2004@gmail.com>
2024-02-24 14:18:08 +0530

    js done

--- script.js
+++ script.js
@@ -39,8 +39,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     function renderBoard(){
         for(let i = 0; i<size;  i++){
             for(let j = 0; j<size; j++){
-                const cell = document.querySelector(`[data-row="${i}"data-col="${j}"]`)
-
+                const cell = document.querySelector(`[data-row="${i}"] [data-column="${j}"]`)
                 const preValue = cell.dataset.value
                 const currentValue = board[i][j]
 
@@ -78,7 +77,87 @@ document.addEventListener('DOMContentLoaded', ()=>{
             }
         }
         if(available.length > 0 ){
-            
+            const randomCell = available[Math.floor(Math.random() * available.length)]
+            board[randomCell.x][randomCell.y] = Math.random() < 0.9 ? 2 : 4
+            const cell = document.querySelector(`[data-row="${randomCell.x}"] [data-column="${randomCell.y}"]`)
+            cell.classList.add('new-tile')
         }
     }
+
+    function move(direction){
+        let hasChanged = false
+        if(direction === 'ArrowUp' || direction === 'ArrowDown'){
+            for(let j = 0; j<size; j++){
+                const column = [...Array(size)].map((_, i)=>board[i][j])
+                const newColumn = transform(column, direction === 'ArrowUp')
+                for (let i = 0; i < size; i++) {
+                    if(board[i][j] !== newColumn[i]){
+                        hasChanged = true
+                        board[i][j] = newColumn[i]
+                    }
+                }
+            }
+        }
+        else if(direction === 'ArrowLeft' || direction === 'ArrowRight'){
+            for (let i = 0; i < size; i++) {
+                const row = board[i]
+                const newRow = transform(row, direction === 'ArrowLeft')
+                if(row.join(',') !== newRow.join(',')){
+                    hasChanged = true
+                    board[i] = newRow
+                }
+            }
+        }
+        if(hasChanged){
+            placeRandom()
+            renderBoard()
+            checkGameOver()
+        }
+    }
+
+    function transform(line, moveTowardsStart){
+        let newLine = line.filter(cell => cell !== 0)
+        if(!moveTowardsStart){
+            newLine.reverse()
+        }
+        for(let i = 0; i<newLine.length - 1; i++){
+            if(newLine[i] === newLine[i+1]){
+                newLine *= 2
+                updateScore(newLine[i])
+                newLine.splice(i + 1, 1)
+            }
+        }
+        while(newLine.length < size){
+            newLine.push(0)
+        }
+        if(!moveTowardsStart){
+            newLine.reverse()
+        }
+        return newLine;
+    }
+
+
+    function checkGameOver(){
+        for(let i = 0; i<size; i++){
+            for(let j = 0; j<size; j++){
+                if(board[i][j] === 0)
+                    return
+                if(j < size-1 && board[i][j] == board[i][j+1])
+                    return
+                if(i < size-1 && board[i][j] == board[i+1][j])
+                return
+            }
+        }
+        gameOverEle.style.display='flex'
+    }
+
+    document.addEventListener('keydown', event=>{
+        if(['ArrowUp'], ['ArrowDown'], ['ArrowLeft'], ['ArrowRight'].includes(event.key)){
+            move(event.key)
+        }
+    })
+
+    document.getElementById('restart-btn').addEventListener('click', restart)
+
+    initializeGame()
 })
